0c1af5d5ba46fc357a4879eb69bf9e643775b587
vladime88 <vladime@gmail.com>
2020-09-28 17:04:30 +0200

    ex

--- src/ex03.js
+++ src/ex03.js
@@ -2,9 +2,8 @@ import fs from 'fs/promises'
 console.log('START OF PROGRAM')
 const copyFile1 = async () => {
     let content1 = await fs.copyFile('./../file1.txt', './../file3.txt')
-    return content1
 }
 
-let content1 = await copyFile1()
+let content = await copyFile1()
 
 console.log('END OF PROGRAM')
